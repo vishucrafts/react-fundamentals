6b267fe5ccc2458f4857bd59f8f5d6fa915968de
vishucrafts <contact@vishu.io>
2023-08-18 17:25:54 +0530

    Use state

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import Timer from './components/Timer';
 
 function App() { 
-	const [time, setTime] = React.useState(0) 
-
-	function increment() {
-		// State is like a photo in that particular call
-		setInterval(() => {
-			// setTime(time + 1)
-			setTime(prevTime => prevTime + 1); 
-		}, 1000);
-	}
-
   return (
     <Timer />
   )
